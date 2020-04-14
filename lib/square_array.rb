f635d8cecaac4f4e7b3c90d9381db4215fab59dd
ILJOO CHAE <9662964@gmail.com>
2020-04-14 00:49:49 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
     counter = 0
     while counter < array.length do
     temp = array[counter]
-    result = temp ** temp
+    result = temp * temp
     temp_array = []
     temp_array.push(result)
     counter+=1
