67f81cef1ff7444cb603f9e3a34e02ad7ccae13a
ILJOO CHAE <9662964@gmail.com>
2020-04-14 00:49:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+
+    counter = 0
+    while counter < array.length do
+    temp = array[counter]
+    result = temp ** temp
+    temp_array = []
+    temp_array.push(result)
+    counter+=1
+    puts temp_array    
+    end
+    
 end
